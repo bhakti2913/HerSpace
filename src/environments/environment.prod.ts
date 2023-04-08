0a8bb0f20b6dcd39270686422e2c2142122f2c4f
Preeti3024 <preetikpati13@gmail.com>
2023-04-08 12:52:37 +0530

    assets & env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    firebase: {
+    
+    apiKey: "AIzaSyChZMJSzLUOIUOADZ-6fuCVHqsE31Zwv84",
+    authDomain: "herspace-8cbad.firebaseapp.com",
+    projectId: "herspace-8cbad",
+    storageBucket: "herspace-8cbad.appspot.com",
+    messagingSenderId: "515435138762",
+    appId: "1:515435138762:web:f2bbf0d547afaf83beae33",
+    measurementId: "G-NTYF41M5NG"
+  
+    },
+    production: true
+  };
+  
