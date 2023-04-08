e5d1da1f5497d84ec5198c247a67e53fafc6410c
Bhakti <bhaktisapariya16@gmail.com>
2023-04-08 23:45:09 +0530

    new changes

--- src/app/JSONdata/navbar.ts
+++ src/app/JSONdata/navbar.ts
@@ -6,36 +6,43 @@ export let navbar = [
     // },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Home",
         "link":"/home",
     },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Tracker",
         "link":"/mood",
     },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Blogs",
         "link":"/blog",
     },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Video",
         "link":"/youtube",
     },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Shop",
         "link":"/product",
     },
     {
         "role":"user",
+        "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Forum",
         "link":"/forum",
     },
     {
         "role":"user",
+        // "image": "https://firebasestorage.googleapis.com/v0/b/women-c.appspot.com/o/assets%2Fimages%2FLogo.png?alt=media&token=64ec5d36-65c0-4283-90c4-27e4edbc3874",
         "name":"Log Out",
         "link":"/signin",
     },
